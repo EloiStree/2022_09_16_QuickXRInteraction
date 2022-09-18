55bdd158c126a5b4d6633bbccc4d06d272faf991
Eloi Stree <eloistree@gmail.com>
2022-09-18 22:25:37 +0200

    Init Save

--- Runtime/Gazable/GazableMonoTag.cs
+++ Runtime/Gazable/GazableMonoTag.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-public class GazeRaySourceMono : MonoBehaviour
+public class GazableMonoTag : MonoBehaviour
 {
-
 }

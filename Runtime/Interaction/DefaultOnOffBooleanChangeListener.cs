78b3f4b7a3ae2fd2ffb121d4aa84ab3d82589449
Eloi Stree <eloistree@gmail.com>
2025-03-18 19:51:26 +0100

    Save: 2025_03_18_19_51

---
+++ Runtime/Interaction/DefaultOnOffBooleanChangeListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DefaultOnOffBooleanChangeListener : MonoBehaviour
+{
+    public bool m_isOn;
+    public UnityEvent<bool> m_onOffUpdate;
+    public UnityEvent m_onOn;
+    public UnityEvent m_onOff;
+    public void SetBoolean(bool value)
+    {
+       
+            m_isOn = value;
+            m_onOffUpdate.Invoke(value);
+            if (value)
+            {
+                m_onOn.Invoke();
+            }
+            else
+            {
+                m_onOff.Invoke();
+            }
+        
+    }
+}
